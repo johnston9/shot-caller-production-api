abada838002df186affaf27cf85b7d6475c0947a
johnston9 <davejohnston9@gmail.com>
2022-08-19 18:23:46 +0000

    Profils views

---
+++ profiles/serializers.py
@@ -0,0 +1,18 @@
+"""
+Serializer for Profiles App
+"""
+from rest_framework import serializers
+from .models import Profile
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    """ Serializer class for Profiles App """
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        """ Meta for Profiles Serializer """
+        model = Profile
+        fields = [
+            'id', 'owner', 'created_at', 'updated_at', 'name',
+            'company', 'content', 'image'
+        ]
---
+++ profiles/urls.py
@@ -0,0 +1,9 @@
+"""
+Urls for Profiles App
+"""
+from django.urls import path
+from profiles import views
+
+urlpatterns = [
+    path('profiles/', views.ProfileList.as_view()),
+]
