f7fc1f83dc4c5e7f6ffdbc8b771857f72483f653
johnston9 <davejohnston9@gmail.com>
2023-06-27 03:50:06 +0000

    Projects error

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -27,6 +27,15 @@ class AccountSerializer(serializers.ModelSerializer):
 
 class ProjectSerializer(serializers.ModelSerializer):
     """ Serializer class for Project """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    owner_name = serializers.ReadOnlyField(source='owner.profile.name')
+    company = serializers.ReadOnlyField(source='owner.profile.company')
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         """ Meta for Project Serializer """
@@ -36,6 +45,15 @@ class ProjectSerializer(serializers.ModelSerializer):
 
 class BudgetSerializer(serializers.ModelSerializer):
     """ Serializer class for Budget """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    owner_name = serializers.ReadOnlyField(source='owner.profile.name')
+    company = serializers.ReadOnlyField(source='owner.profile.company')
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         """ Meta for Budget Serializer """
