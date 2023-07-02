dfd9610d05d81bcae2bcb348bc0ab1f5a429f805
johnston9 <davejohnston9@gmail.com>
2023-07-02 20:01:23 +0000

    Interger fields on Budget model

--- accounts/models.py
+++ accounts/models.py
@@ -60,25 +60,21 @@ class Budget(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    # details
     title = models.CharField(max_length=25, blank=True)
     series = models.CharField(max_length=25, blank=True)
     prodco = models.CharField(max_length=25, blank=True)
     format = models.CharField(max_length=25, blank=True)
     location = models.CharField(max_length=25, blank=True)
     dated = models.CharField(max_length=25, blank=True)
-    # length
     research = models.IntegerField(blank=True)
     prep = models.IntegerField(blank=True)
     shoot = models.IntegerField(blank=True)
     wrap = models.IntegerField(blank=True)
     post = models.IntegerField(blank=True)
     length_total = models.IntegerField(blank=True)
-    # ABOVE THE LINE
-    # rights
-    story_rights = models.CharField(max_length=25, blank=True)
-    miscellaneous = models.CharField(max_length=25, blank=True)
-    rights_total = models.CharField(max_length=25, blank=True)
+    story_rights = models.IntegerField(blank=True)
+    miscellaneous = models.IntegerField(blank=True)
+    rights_total = models.IntegerField(blank=True)
 
     class Meta:
         """ Meta for ordering """
