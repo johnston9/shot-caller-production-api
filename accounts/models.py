2f710e518f66c2adbd7c701926e3c6cdc08fd85b
johnston9 <davejohnston9@gmail.com>
2023-07-02 23:41:41 +0000

    Account

--- accounts/models.py
+++ accounts/models.py
@@ -56,7 +56,7 @@ class Budget(models.Model):
     """
     Budget model
     """
-    project = models.OneToOneField(Project, on_delete=models.CASCADE)
+    project = models.CharField(max_length=25, blank=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
