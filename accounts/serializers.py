c11982b2a3239531689ce31f6542bfd12cfcaf00
johnston9 <davejohnston9@gmail.com>
2023-07-02 20:06:49 +0000

    delete Budget

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,7 +4,7 @@ Serializer for the Accounts App
 from rest_framework import serializers
 from .models import Account
 from .models import Project
-from .models import Budget
+# from .models import Budget
 
 
 class AccountSerializer(serializers.ModelSerializer):
@@ -40,22 +40,4 @@ class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         """ Meta for Project Serializer """
         model = Project
-        fields = '__all__'
-
-
-class BudgetSerializer(serializers.ModelSerializer):
-    """ Serializer class for Budget """
-    owner = serializers.ReadOnlyField(source='owner.username')
-    is_owner = serializers.SerializerMethodField()
-    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
-    owner_name = serializers.ReadOnlyField(source='owner.profile.name')
-    company = serializers.ReadOnlyField(source='owner.profile.company')
-
-    def get_is_owner(self, obj):
-        request = self.context['request']
-        return request.user == obj.owner
-
-    class Meta:
-        """ Meta for Budget Serializer """
-        model = Budget
         fields = '__all__'
