92876777c5ba9048f6fdf5361f3cdcc94ad424f6
johnston9 <davejohnston9@gmail.com>
2023-07-03 20:17:52 +0000

    Changed project field to char

--- budgets/models.py
+++ budgets/models.py
@@ -10,7 +10,7 @@ class Budget(models.Model):
     """
     Budget model
     """
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    project = models.CharField(max_length=25, blank=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
